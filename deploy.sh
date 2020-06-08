50b2bd991c46ed617869b78259796ae4315a86d9
Syahkun <pamungkas@alterra.id>
2020-06-08 03:45:03 -0400

    tes

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh-add -k ~/.ssh/id_rsa &&
 
 source ~/.profile
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-sudo docker stop backend
-sudo docker rm backend
+sudo docker stop flask-tutorial
+sudo docker rm flask-tutorial
 sudo docker rmi syahkun/flask-tutorial:latest
 sudo docker run -d --name flask-tutorial -p 5000:5000 syahkun/flask-tutorial:latest
